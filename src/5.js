cd284a01f6ae8716a463eed4297815f39fe5a154
ekaterinavolkov127 <ekaterinavolkov127@gmail.com>
2025-03-09 18:48:23 +0000

    Add 5.js

---
+++ src/5.js
@@ -0,0 +1,5 @@
+// Get a random number between 1 and 10
+const randomNumber = Math.floor(Math.random() * 10) + 1;
+
+// Print the result to the console
+console.log(`The random number is ${randomNumber}`);
