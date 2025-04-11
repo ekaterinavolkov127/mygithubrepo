040d03a5a88ef52c5332907f8e4b1cb8ea01537b
ekaterinavolkov127 <ekaterinavolkov127@gmail.com>
2025-04-11 12:41:43 +0000

    Add 40.js

---
+++ src/40.js
@@ -0,0 +1,8 @@
+// Create an array of numbers
+const numbers = [1, 2, 3, 4, 5];
+
+// Calculate the sum of all numbers in the array
+let sum = numbers.reduce((acc, curr) => acc + curr, 0);
+
+// Print the sum to the console
+console.log(sum);
